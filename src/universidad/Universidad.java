cc2a503aeea5a77bc582716677807f5ed622ed2c
Dayanacaicedo <dayana.caicedo@mariaelvinia.edu.co>
2019-10-09 15:21:19 -0500

    version 1.2
    Sbrecarga de Metodos

--- src/universidad/Universidad.java
+++ src/universidad/Universidad.java
@@ -1,13 +1,31 @@
 package universidad;
-//buenas tardes
+
 import java.util.Date;
 
 public class Universidad {
 
     private Date fechaDeInauguracion;
     private String nombre;
-    private String nombreDelRector;
+    private String nombresDeEstudiantes;
     private boolean seguroEstudiantil;
     private int numeroDeSalones;
 
+    
+    
+    public void estudiar(int cantidadDeAulas,String segundoNombreDeEstudiantes){
+            if(cantidadDeAulas<0){
+                   System.out.println("No hay clases");
+            }else{
+                if(numeroDeSalones<cantidadDeAulas){
+                    System.out.println("No hay clases");
+                }else{
+                    numeroDeSalones = cantidadDeAulas;
+                    nombresDeEstudiantes = segundoNombreDeEstudiantes;
+                    
+                           
+                }
+              
+            }
+        
+    }
 }
