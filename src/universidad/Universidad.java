1b0763ad5cb532f0e1ec9268aab9ce12881e7919
Dayanacaicedo <dayana.caicedo@mariaelvinia.edu.co>
2019-10-09 14:21:45 -0500

    version 1.0
    Creación de 5 atributos de clase

---
+++ src/universidad/Universidad.java
@@ -0,0 +1,13 @@
+package universidad;
+
+import java.util.Date;
+
+public class Universidad {
+
+    private Date fechaDeInauguracion;
+    private String nombre;
+    private String nombreDelRector;
+    private boolean seguroEstudiantil;
+    private int numeroDeSalones;
+
+}
